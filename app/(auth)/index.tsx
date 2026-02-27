dd9e012e05219a3f7c896692e4d2f035accdefc2
taptrustbk-cyber <taptrust.bk@gmail.com>
2026-02-27 06:14:25 +0300

    Create index.tsx

---
+++ app/(auth)/index.tsx
@@ -0,0 +1,8 @@
+import { View, Text } from 'react-native';
+export default function AuthIndex() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>/auth is working ✅</Text>
+    </View>
+  );
+}
