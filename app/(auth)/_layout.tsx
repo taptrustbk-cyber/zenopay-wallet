7ffe59fe424013de3130fd6b0f0c2f1463858d34
taptrustbk-cyber <taptrust.bk@gmail.com>
2026-02-27 06:24:13 +0300

    Update _layout.tsx

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -3,7 +3,7 @@ import { Stack } from 'expo-router';
 export default function RootLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
-      {/* ✅ allow /auth/* pages */}
+      {/* ✅ REQUIRED: enable /auth/* routes */}
       <Stack.Screen name="auth" options={{ headerShown: false }} />
     </Stack>
   );
