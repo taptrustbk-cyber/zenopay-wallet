44ce1c1c8ac118e0ee9ac6481315863060d101f3
taptrustbk-cyber <taptrust.bk@gmail.com>
2026-03-22 04:25:11 +0300

    Update profile.tsx

--- app/(app)/profile.tsx
+++ app/(app)/profile.tsx
@@ -13,6 +13,7 @@ import {
   Pressable,
 } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { LinearGradient } from 'expo-linear-gradient';
 import { Ionicons } from '@expo/vector-icons';
 import { useRouter, Stack, useFocusEffect } from 'expo-router';
 import { useMutation } from '@tanstack/react-query';
